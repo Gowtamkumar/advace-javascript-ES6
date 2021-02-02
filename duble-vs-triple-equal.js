101ec6a14a8eb47df9d022467b1a11efbdce6571
Gowtamkumar <gowtampaul0@gmail.com>
2021-02-02 07:43:26 +0600

    advance javascript

---
+++ duble-vs-triple-equal.js
@@ -0,0 +1,16 @@
+const first = 5;
+const second = "5";
+
+if (first == second) {
+    console.log("condition is true");
+}else{
+    console.log("condition is fales");
+}
+
+//=== equal example
+if (first === second) {
+    console.log("condition  is true (tribole equal)");
+}else{
+
+    console.log("condition is fales (tribole equal)");
+}
---
+++ letconst.js
@@ -0,0 +1,7 @@
+const janu = "Poly janu";
+console.log(janu);
+const numbers = [12, 20];
+
+numbers.push(23);
+// 
+console.log(numbers);
